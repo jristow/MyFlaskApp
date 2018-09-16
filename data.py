c2cec4212cd446e64a2ba4034ff1cb1832aae386
Jordan Ristow <jristow@gmail.com>
2018-09-16 08:16:15 -0700

    Init
    Initial commit.

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Random text goes into here',
+            'author': 'Jordan Ristow',
+            'create_date' : '09-15-2018'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Random text goes into here',
+            'author': 'Jordan Ristow',
+            'create_date' : '09-15-2018'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Random text goes into here',
+            'author': 'Jordan Ristow',
+            'create_date' : '09-15-2018'
+        }
+    ]
+
+    return articles 
